9acc537b9e25e30117e76142f61b9bf8e44f39b1
Giuseppe Vigneri <geppe1990@gmail.com>
2021-07-07 11:10:36 +0200

    Articles + bulma

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import React from "react";
 import { hot } from "react-hot-loader/root";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/home/home";
+import "bulma/bulma.sass";
 
 const App = () => {
 	return (
--- src/components/articlesList/articlesList.js
+++ src/components/articlesList/articlesList.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "./articlesList.scss";
 import { get } from "../../helpers";
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 const ArticlesList = () => {
 	// const [errorMessage, setErrorMessage] = useState("");
 	const [articles, setArticles] = useState([]);
 	// const [title, setTitle] = useState("");
 
+	console.log(articles);
 	useEffect(() => {
 		get(
-			"https://www.reddit.com/r/wallpapers/top.json?limit=30&t=day",
+			"https://www.reddit.com/r/reactjs/top.json?limit=15&t=day",
 			(response) => {
 				setArticles(response.data.children);
 			} // setErrorMessage
@@ -17,15 +19,35 @@ const ArticlesList = () => {
 	}, []);
 
 	return (
-		<div className="ArticlesList">
-			<h2>Wallpapers</h2>
-			{articles.length > 0 ? (
-				<ul>
-					{articles.map((key, index) => (
-						<li key={index}> {key.data.title}</li>
-					))}
-				</ul>
-			) : null}
+		<div className="ArticlesList section">
+			<div className="container">
+				<h2 className="title">Wallpapers</h2>
+				{articles.length > 0 ? (
+					<ul>
+						{articles.map((key) => (
+							<li key={key.data.id} className="mb-6">
+								<div className="card">
+									<div className="card-content">
+										<div className="is-flex is-justify-content-space-between is-align-content-center">
+											<h3 className="is-inline m-0">
+												{key.data.title}
+											</h3>
+											<a
+												href={`https://reddit.com${key.data.permalink}`}
+												target="_blank"
+												rel="noreferrer"
+											>
+												<FaExternalLinkAlt />
+											</a>
+										</div>
+										<p>{key.data.selftext}</p>
+									</div>
+								</div>
+							</li>
+						))}
+					</ul>
+				) : null}
+			</div>
 		</div>
 	);
 };
